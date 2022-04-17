7c93410fcfdd082a23c9ddd61f4ab397a31ce30d
solar05 <artem00298@gmail.com>
2022-04-17 23:55:28 +0300

    Add to hex

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule Descisionex.MixProject do
       app: :descisionex,
       version: "0.1.0",
       elixir: "~> 1.11",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Descisionex",
+      source_url: "https://github.com/solar05/descisionex"
     ]
   end
 
@@ -23,7 +28,22 @@ defmodule Descisionex.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:matrix, "~> 0.3.2"}
+      {:matrix, "~> 0.3.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+
+  defp description() do
+    "Library for dealing with descision theory algorithms."
+  end
+
+  defp package() do
+    [
+      name: "descisionex",
+      files: ~w(lib mix.exs README* LICENSE* LICENSE* .formatter.exs),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/solar05/descisionex"}
     ]
   end
 end
